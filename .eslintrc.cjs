760ab8bc0d9edfc6e93b95168fb452525cc4ecd1
Alan Paccor <alanpaccor@gmail.com>
2024-10-10 22:21:35 -0400

    attempt 9 of vercel deployment issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,5 +5,8 @@ module.exports = {
     'react/prop-types': 'off',
     'react/jsx-key': 'error',
     'react/no-unescaped-entities': 'error',
+    'no-unused-vars': 'warn',
+    '@next/next/no-img-element': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
