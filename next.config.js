f59f7fbc53aac313fc27e72a9ac6bb06856e6783
Alan Paccor <alanpaccor@gmail.com>
2024-10-14 19:03:19 -0400

    deployed site visual issue 2

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Add any other necessary configurations
+  // Make sure there are no redirects or rewrites here that might interfere
 };
 
 export default nextConfig;
--- src/app/generators/ArticleGen/page.tsx
+++ src/app/generators/ArticleGen/page.tsx
@@ -1,13 +1,20 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import { SecondHeader } from '@/app/sections/SecondHeader';
 import Footer from '@/app/sections/Footer';
 
-const UploadPage = dynamic(() => import('./upload'), { ssr: false });
+const UploadPage = dynamic(() => import('./upload'), { 
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 const ArticleGenPage: React.FC = () => {
+  useEffect(() => {
+    console.log('ArticleGenPage mounted');
+  }, []);
+
   return (
     <div>
       <SecondHeader />
