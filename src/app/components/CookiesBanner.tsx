fa77c0586387320db904b4954dd0e7c7fed8fc8e
Alan Paccor <alanpaccor@gmail.com>
2024-10-14 15:17:47 -0400

    attempt 36 vercel deployment issues

--- src/app/components/CookiesBanner.tsx
+++ src/app/components/CookiesBanner.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import { Button, Container, Text, Group } from '@mantine/core';
+import { Button, Container, Text, Flex } from '@mantine/core';
 import { useLocalStorage } from '@mantine/hooks';
 
 export function CookiesBanner() {
@@ -20,18 +20,17 @@ export function CookiesBanner() {
 
   return (
     <div
-      className={`fixed bottom-0 transition-opacity duration-600 ${fadeIn ? 'opacity-100' : 'opacity-0'} bg-gray-800 text-white p-4 rounded-lg shadow-lg z-50 lg:w-[20vw] md:w-[35vw] lg:right-4 md:right-4 m-4 sm:bottom-0
-`}
+      className={`fixed bottom-0 transition-opacity duration-600 ${fadeIn ? 'opacity-100' : 'opacity-0'} bg-gray-800 text-white p-4 rounded-lg shadow-lg z-50 lg:w-[20vw] md:w-[35vw] lg:right-4 md:right-4 m-4 sm:bottom-0`}
     >
       <Container>
-        <Group position="apart" noWrap>
-          <Text className="text-sm">
+        <Flex justify="space-between" align="center" wrap="nowrap">
+          <Text className="text-sm mr-2">
             This website uses cookies to enhance your experience. By continuing to visit this site you agree to our use of cookies.
           </Text>
           <Button onClick={() => setVisible(false)} className="btn hover:bg-blue-700 text-white">
             I Agree
           </Button>
-        </Group>
+        </Flex>
       </Container>
     </div>
   );
