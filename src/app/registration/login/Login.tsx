bb4f1f5f74d907de062c86a60d7166fcf85acce6
Alan Paccor <alanpaccor@gmail.com>
2024-10-15 11:39:43 -0400

    fixed login page 'auth' variable again

--- src/app/registration/login/Login.tsx
+++ src/app/registration/login/Login.tsx
@@ -5,7 +5,10 @@ import { useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react';
 import { signInWithEmailAndPassword, onAuthStateChanged, Auth } from 'firebase/auth';
 import Link from 'next/link';
-import { auth } from '@/firebase/firebaseApp';
+import { auth as firebaseAuth } from '@/firebase/firebaseApp';
+
+// Type assertion
+const auth = firebaseAuth as Auth;
 
 export default function Login() {
   const [passwordVisible, setPasswordVisible] = useState(false);
