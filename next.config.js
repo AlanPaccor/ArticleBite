aeafafbeeb6cd6c00a7dcbb44eff76463c87d1c0
Alan Paccor <alanpaccor@gmail.com>
2024-10-15 17:57:42 -0400

    trying to fix the gen page crash

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API_URL: process.env.API_URL,
+  publicRuntimeConfig: {
+    FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   },
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,14 +1,17 @@
 import { FirebaseApp, initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_APP_ID
+  apiKey: publicRuntimeConfig.FIREBASE_API_KEY,
+  authDomain: publicRuntimeConfig.FIREBASE_AUTH_DOMAIN,
+  projectId: publicRuntimeConfig.FIREBASE_PROJECT_ID,
+  storageBucket: publicRuntimeConfig.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: publicRuntimeConfig.FIREBASE_MESSAGING_SENDER_ID,
+  appId: publicRuntimeConfig.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
