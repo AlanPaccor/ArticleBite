07d111440af483c992f4b1b688dd0250d60cc09c
Alan Paccor <alanpaccor@gmail.com>
2024-10-15 11:53:26 -0400

    fixed firebase config and app

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,8 +1,8 @@
-import { initializeApp, getApps, FirebaseApp } from 'firebase/app';
+import { FirebaseApp, initializeApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_REACT_APP_FIREBASE_PROJECT_ID,
